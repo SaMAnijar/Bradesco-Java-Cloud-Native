cdfa3e8bb8e7c4b546eab8ac3b7fe6c760963c56
Samuel Anijar <samuelb.anijar@gmail.com>
2025-04-21 12:46:34 -0300

    desafio

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) throws Exception {
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1+numero2); }
 }
